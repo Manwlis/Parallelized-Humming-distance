b7721d6e4c0e95588b3ab8d57f13c6566462e85f
manolios7 <33401961+manolios7@users.noreply.github.com>
2018-07-04 20:54:42 +0300

    Update pthread_line.c

--- code/pthread_line.c
+++ code/pthread_line.c
@@ -2,9 +2,8 @@
  ============================================================================
  Name        :	pthread_line.c
  Author      :	Manolis P, Dimitris K
- Description :	Task is set as: Compare one string from group A with all the strings from group B.
- 		For each different character add 1 to the apropriate diffs[i][j]. 
- 		Then each thread has to acumulate and add the lineDiff to the total diff.
+ Description :	Task is set as: Compare one string from group A with all the strings from group B. For each different
+ character add 1 to the apropriate diffs[i][j]. Then each thread has to acumulate and add the lineDiff to the total diff.
  ============================================================================
  */
 
